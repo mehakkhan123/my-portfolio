a4897676d34cb619ba645cea96d632129542d122
mehakkhan123 <mahekkhan284@gmail.com>
2025-07-15 17:49:42 +0500

    add escape entity

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,7 +1,14 @@
 import React from 'react'
 
-export default function page() {
+export default function ProjectsPage() {
   return (
-    <div>page</div>
+    <div className='flex flex-col items-center'>
+      <div className='flex flex-col items-center w-full p-6 max-w-[1536px] bg-gray-50'>
+          <h1 className='text-3xl pb-2 md:text-4xl lg:text-5xl font-bold text-gray-700'>About Me</h1>
+          <hr className='w-[96%] h-1  border-t-yellow-400 bg-yellow-400'/>
+          
+        </div>
+
+    </div>
   )
 }
